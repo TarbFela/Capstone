235cf2a5b3d5bcd0ff1da8f87fd0aadce9283e6c
Maxim LaPlante <maxim.laplante@maine.edu>
2026-04-12 22:28:32 -0400

    [oops] Added mcp pio files *back* to git!

---
+++ src2/mcp3x6xR_driver/mcp_pio.h
@@ -0,0 +1,28 @@
+#ifndef MCP_PIO_H
+#define MCP_PIO_H
+
+#include "hardware/pio.h"
+#include "hardware/gpio.h"
+
+#include "mcp3x6xR.h"
+
+// size of each DMA's buffer.
+#define DMA_BUFF_SIZE 64
+
+// Globally-accessible array for MCP/SIO/DMA data (i.e. readings). Aligned for DMA Ring mode.
+extern uint32_t dma_buff[DMA_BUFF_SIZE*2];
+
+typedef struct mcp_pio_t {
+    PIO pio;
+    uint sm;
+    mcp_info_t *mcp_info;
+    uint dma_a;
+    uint dma_b;
+    uint32_t *buff;
+} mcp_pio_t;
+
+void mcp_pio_init(mcp_pio_t *s,mcp_info_t *mcp,uint32_t *sample_buff, void (*dma_handler)(void));
+void mcp_pio_start(mcp_pio_t *s);
+void mcp_pio_stop(mcp_pio_t *s);
+
+#endif
